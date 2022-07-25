607fbd2cba251cd0116975cbc391ed5555a1d6fe
이근희 <report2829@naver.com>
2022-07-25 14:02:34 +0900

    front 기본작업

---
+++ frontend/src/router/index.js
@@ -0,0 +1,59 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '../views/LoginView.vue'
+import SignupView from '../views/SignupView.vue'
+import LobbyView from '../views/LobbyView.vue'
+import WaitView from '../views/WaitView.vue'
+import IngameView from '../views/IngameView.vue'
+import FinalView from '../views/FinalView.vue'
+import MyProfileView from '../views/MyProfileView.vue'
+import MyFriendView from '../views/MyFriendView.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'login',
+    component: LoginView
+  },
+  {
+    path: '/signup',
+    name: 'signup',
+    component: SignupView
+  },
+  {
+    path: '/lobby',
+    name: 'lobby',
+    component: LobbyView
+  },
+  {
+    path: '/:roomnumber/wait',
+    name: 'wait',
+    component: WaitView
+  },
+  {
+    path: '/:roomnumber/ingame',
+    name: 'ingame',
+    component: IngameView
+  },
+  {
+    path: '/:roomnumber/final',
+    name: 'final',
+    component: FinalView
+  },
+  {
+    path: '/:id_pk/profile',
+    name: 'profile',
+    component: MyProfileView
+  },
+  {
+    path: '/:id_pk/friend',
+    name: 'friend',
+    component: MyFriendView
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes
+})
+
+export default router
