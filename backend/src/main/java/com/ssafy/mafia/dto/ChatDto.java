06a5ccceab26c37efb53287efa4ee2b1e3e4c49c
GownMinYong <alsdyd320@naver.com>
2022-08-01 13:57:46 +0900

    BE socket chat part 수정

--- backend/src/main/java/com/ssafy/mafia/dto/ChatDto.java
+++ backend/src/main/java/com/ssafy/mafia/dto/ChatDto.java
@@ -2,9 +2,11 @@ package com.ssafy.mafia.dto;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class ChatDto {
 	 // 유저의 닉네임을 저장하기 위한 변수
     private String nickName;
