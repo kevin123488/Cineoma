8d6ad7f25efccf634fbc3a8ece6c750cd788efbc
박정현 <kevin123488@naver.com>
2022-08-01 13:40:14 +0900

    parkjeonghyun_intro_page

--- frontend/src/views/LoginView.vue
+++ frontend/src/views/LoginView.vue
@@ -5,8 +5,8 @@
 <!--  -->
 
 <!-- 여기 div 추가해서 일단 맛만 보겠음 -->
-<div class="intro-background">
-    <section class="u-align-center u-clearfix u-image u-section-2" id="carousel_bb13" data-image-width="1920" data-image-height="1080">
+<!-- <div class="intro-background"> -->
+    <!-- <section class="u-align-center u-clearfix u-image u-section-2" id="carousel_bb13" data-image-width="1920" data-image-height="1080">
       <div class="u-clearfix u-sheet u-valign-middle u-sheet-1">
         <div class="u-align-center u-container-style u-expanded-width-md u-expanded-width-sm u-expanded-width-xs u-group u-shape-rectangle u-group-1">
           <div class="u-container-layout u-valign-middle u-container-layout-1">
@@ -28,9 +28,9 @@
           </div>
         </div>
       </div>
-    </section>
+    </section> -->
 
-</div>
+<!-- </div> -->
 
 
 
@@ -43,17 +43,20 @@
 
 
 <!-- 디자인 전 로그인 코드 -->
-<!-- <div>
-  <h1>Login</h1>
-  <div class="loginForm">
-    <input type="text" placeholder="아이디 입력" v-model="userId">
-    <input type="text" placeholder="비밀번호 입력" v-model="userPw">
+<div class="intro-background">
+  <div class="login-background">
+    <h1>Login</h1>
+    <div class="loginForm">
+      <input type="text" placeholder="아이디 입력" v-model="userId">
+      <input type="text" placeholder="비밀번호 입력" v-model="userPw">
+    </div>
+    <div>
+      <button class="loginSubmit" @click="login">login</button>
+      <button class="goToSignup" @click="goToSignup">회원가입</button>
+    </div>
   </div>
-  <div>
-    <button class="loginSubmit" @click="login">login</button>
-    <button class="goToSignup" @click="goToSignup">회원가입</button>
-  </div>
-</div> -->
+<div class="start-btn" @click="startGame"><button class="start-btn-btn">start!!</button></div>
+</div>
 </template>
 
 <script>
@@ -65,10 +68,13 @@ export default {
         return {
             userId: null,
             userPw: null,
+            position: 1000,
         }
     },
-
     methods: {
+        startGame() {
+
+        },
         async login() { // 프로미스 이용
             let loginObj = {
                 email: null,
@@ -113,10 +119,37 @@ export default {
         goToSignup() {
             this.$router.push("/signup")
         },
-    }
+    },
+    mounted () {
+        // window.scrollTo({top:2000, behavior:"instant"})
+        // let step;
+        // for (step = 0; step < 1000; step++) {
+        //     setTimeout(() => {
+        //         console.log("쉬어")
+        //     }, 10)
+        //     console.log("되나?")
+        //     window.scrollTo({top:2000-step, behavior:"smooth"})
+        // }
+    },
+
+
 }
 </script>
 
 <style>
+.intro-background {
+    background-image: url(../../public/homedesign/images/intro_movie.mp4);
+    background-repeat: no-repeat;
+    background-size: cover;
+    height: 2000px;
+}
+.start-btn {
+    background-color: white;
+    margin: auto;
+    width: 10%;
 
+}
+.start-btn-btn {
+
+}
 </style>
