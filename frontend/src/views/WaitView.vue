aed8170582daef5e6d01698c14460695d59582bf
이근희 <report2829@naver.com>
2022-08-04 16:15:05 +0900

    ingame, wait

--- frontend/src/views/WaitView.vue
+++ frontend/src/views/WaitView.vue
@@ -87,10 +87,24 @@
     </div>
 
   </div>
+  <div>
+    <div
+      v-for="(item, idx) in recvList"
+      :key="idx"
+    >
+      <h3>유저이름: {{ item.nickName }}</h3>
+      <h3>내용: {{ item.content }}</h3>
     </div>
 
-
-
+    <input
+      v-model="message"
+      type="text"
+      @keyup="sendMessage"
+    >
+  </div>
+  
+  
+  </div>
     <h1>대기방임</h1>
     <hr>
     <router-link :to="{ name: 'ingame', params: { id_pk: num } }" 
@@ -99,56 +113,110 @@
 </template>
 
 <script>
-  import { mapGetters, mapActions } from 'vuex'
-  const roomdataStore = "roomdataStore"
-  const memberStore = "memberStore"
+import { mapGetters, mapActions } from 'vuex'
+const roomdataStore = "roomdataStore"
+const memberStore = "memberStore"
 
-  export default {
+import Stomp from 'webstomp-client'
+import SockJS from 'sockjs-client'
 
-    components: {
+export default {
+
+  components: {
+  },
+  data() {
+    return {
+      num : 1,
+      nickName: 'a',
+      message: '',
+      recvList: []
+    }
+  },
+  
+  computed: {
+    ...mapGetters(roomdataStore, [
+      'roomNo',
+      'roomTitle',
+      'isCaptain'
+    ]),
+    ...mapGetters(memberStore, [
+      'isLogin',
+    ]),
+    ...mapActions(roomdataStore, [
+      'deleteRoom',
+      'enterRoom',
+      'saveRoomTitle',
+      'saveIsCaptain',
+    ])
+  },
+
+  created() {
+    // App.vue가 생성되면 소켓 연결을 시도합니다.
+    this.connect()
+    console.log(this.isCaptain)
+    console.log(this.roomNo)
+  },
+
+  methods: {
+    tryDeleteRoom() {
+      this.deleteRoom(this.roomNo)
     },
-    data() {
-      return {
-        num : 1,
+    tryLeaveRoom() {
+    // roomInfo = { roomNo: int, password: { password: int } }
+    const roomInfo = {
+      roomNo: this.roomNo,
+      password: 0,
+    }
+    this.enterRoom(roomInfo)
+    },
+    connect() {
+      const serverURL = "https://i7e107.p.ssafy.io:8080"
+      let socket = new SockJS(serverURL);
+      this.stompClient = Stomp.over(socket);
+      console.log(`소켓 연결을 시도합니다. 서버 주소: ${serverURL}`)
+      this.stompClient.connect(
+        {},
+        frame => {
+          // 소켓 연결 성공
+          this.connected = true;
+          console.log('소켓 연결 성공', frame);
+          // 서버의 메시지 전송 endpoint를 구독합니다.
+          // 이런형태를 pub sub 구조라고 합니다.
+          this.stompClient.subscribe("/sendChat/", res => {
+            console.log('구독으로 받은 메시지 입니다.', res.body);
+
+            // 받은 데이터를 json으로 파싱하고 리스트에 넣어줍니다.
+            this.recvList.push(JSON.parse(res.body))
+          });
+        },
+        error => {
+          // 소켓 연결 실패
+          console.log('소켓 연결 실패', error);
+          this.connected = false;
+        }
+      );        
+    },
+    send() {
+      console.log("Send message:" + this.message);
+      if (this.stompClient && this.stompClient.connected) {
+        const msg = { 
+          roomNo: this.roomNo,
+          nickName: this.nickName,
+          content: this.message 
+        };
+        this.stompClient.send(`/receiveChat/${this.roomNo}`, JSON.stringify(msg), {});
       }
     },
-    
-    computed: {
-      ...mapGetters(roomdataStore, [
-        'roomNo',
-        'roomTitle',
-        'isCaptain'
-      ]),
-      ...mapGetters(memberStore, [
-        'isLogin',
-      ]),
-      ...mapActions(roomdataStore, [
-        'deleteRoom',
-        'enterRoom',
-        'saveRoomTitle',
-        'saveIsCaptain',
-      ])
-    },
-
-    methods: {
-      tryDeleteRoom() {
-        this.deleteRoom(this.roomNo)
-      },
-      tryLeaveRoom() {
-      // roomInfo = { roomNo: int, password: { password: int } }
-      const roomInfo = {
-        roomNo: this.roomNo,
-        password: 0,
+    sendMessage (e) {
+      if(e.keyCode === 13 && this.nickName !== '' && this.message !== ''){
+        this.send()
+        this.message = ''
       }
-      this.enterRoom(roomInfo)        
-      },      
-    },
-    created() {
-      console.log(this.isCaptain)
-    },
-    mounted() {
     }
-    }
+  },
+  mounted() {
+  }
+}
 </script>
 
 <style>
