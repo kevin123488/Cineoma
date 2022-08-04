d59c51cd14a3b3521e2ffa0fbd1617a242db498d
mraz0210 <48303994+hansol620915@users.noreply.github.com>
2022-08-04 14:24:22 +0900

    BE - FolllowController 수정
    FolllowController 파라미터 입력 양식 수정

--- backend/src/main/java/com/ssafy/mafia/controller/FollowController.java
+++ backend/src/main/java/com/ssafy/mafia/controller/FollowController.java
@@ -9,6 +9,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,8 +33,8 @@ public class FollowController {
 	private static final String FAIL = "fail";
 	
 	@ApiOperation(value = "친구 목록 조회")
-	@GetMapping(value = "/{userid}")
-	public ResponseEntity<List<User>> followList(@RequestParam("id") String id) throws Exception {
+	@GetMapping(value = "{id}")
+	public ResponseEntity<List<User>> followList(@PathVariable String id) throws Exception {
 		return new ResponseEntity<List<User>>(followService.followList(id), HttpStatus.OK);
 	}
 
