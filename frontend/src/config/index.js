7e5625d6b0a0db95ce139d01aed8ae9e07af743a
박제학 <wpgkr1093@naver.com>
2022-08-05 13:38:43 +0900

    박제학_로비친구띄우기작업중

--- frontend/src/config/index.js
+++ frontend/src/config/index.js
@@ -1,4 +1,5 @@
-const API_BASE_URL = "http://i7e107.p.ssafy.io"; // 일단 이 포트로
-// const API_BASE_URL = "http://localhost:8080"; // 일단 이 포트로
+// const API_BASE_URL = "https://i7e107.p.ssafy.io"; // 일단 이 포트로
+const API_BASE_URL = "http://localhost:8080"; // 일단 이 포트로
+// const API_BASE_URL = ""; // 일단 이 포트로
 
 export { API_BASE_URL };
