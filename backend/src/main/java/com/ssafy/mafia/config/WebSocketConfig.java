ebe7d9fe4dfd0e18c227b3ed649891f767b8f881
GownMinYong <alsdyd320@naver.com>
2022-08-08 10:26:50 +0900

    webSocket config 업데이트

--- backend/src/main/java/com/ssafy/mafia/config/WebSocketConfig.java
+++ backend/src/main/java/com/ssafy/mafia/config/WebSocketConfig.java
@@ -13,18 +13,17 @@ import lombok.RequiredArgsConstructor;
 @EnableWebSocketMessageBroker
 public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
-    @Override
+   @Override
     // 클라이언트가 메시지를 구독할 endpoint를 정의합니다.
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/sendChat");
+        config.enableSimpleBroker("/topic");
 //        config.setApplicationDestinationPrefixes("/app");
     }
 
     @Override
     // connection을 맺을때 CORS 허용합니다.
     public void registerStompEndpoints(StompEndpointRegistry registry) {
-        registry.addEndpoint("/")//소켓 연결용 uri
+        registry.addEndpoint("/roomSocket","/mafiaSocket")//소켓 연결용 uri
             .setAllowedOriginPatterns("http://localhost"+"*","https://i7e107.p.ssafy.io"+"*")//cros 허용 url
             .withSockJS();
     }
-}
