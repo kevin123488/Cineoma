b09aa2a9c56660f02122c6047d14f8c12732f963
박제학 <wpgkr1093@naver.com>
2022-08-12 11:09:04 +0900

    마스터수정

--- frontend/src/views/IngameView.vue
+++ frontend/src/views/IngameView.vue
@@ -1,5 +1,58 @@
 <template>
 <div :class="{'ingameNight': progress.isNight, 'ingameDay': progress.isDay, 'ingameDayVote': progress.isVoteDay, 'ingameDayVoteResult': progress.isVoteDayResult, 'ingameNightResult': progress.isNightResult}">
+  
+  <!-- 직업확인 두루마리 -->
+  <button @click="openJobRoll" :class="{ jobRollFormSide: !isjobRollCenter, jobRollFormCenter: isjobRollCenter }">
+    <div class="voteItem">
+    </div>
+  </button>
+
+  <!-- 직업확인창 -->
+  <button v-if="isJobRollOpen" @click="switchJobRoll" class="openedJobRoll">
+
+    <!-- 마피아 -->
+    <div v-if="job === 'mafia'">
+      <div class="mafiaImage"></div>        
+      <div style="width: 65%; margin-left: 120px; margin-top: 390px;">
+        <h3 class="">당신의 직업: 마피아</h3>
+        <h3 class="">밤마다 한명의 플레이어를 죽일 수 있습니다.</h3>
+        <h3 class="">모든 플레이어를 죽이면 승리!</h3>
+      </div>  
+    </div>  
+
+    <!-- 의사 -->
+    <div v-if="job === 'doctor'">
+      <div class="doctorImage"></div>  
+      <div style="width: 65%; margin-left: 120px; margin-top: 390px;">
+        <h3 class="">당신의 직업: 의사</h3>
+        <h3 class="">밤마다 한명의 플레이어를 마피아로 부터 보호할 수 있습니다.</h3>
+        <h3 class="">마피아를 잡아내면 승리!</h3>
+      </div>  
+    </div>  
+
+    <!-- 교주 -->
+    <div v-if="job === 'headmaster'">
+      <div class="headmasterImage"></div>      
+      <div style="width: 65%; margin-left: 120px; margin-top: 390px;">
+        <h3 class="">당신의 직업: 교주</h3>
+        <h3 class="">특정 동작을 수행할 때 마다 신자 카운트가 증가합니다.</h3>
+        <h3 class="">다른 플레이어 수 만큼 신자 카운트를 쌓으면 승리!</h3>
+      </div>  
+    </div>
+
+    <!-- 시민 -->
+    <div v-if="job === 'citizen'">
+      <div class="citizenImage"></div>
+      <div style="width: 65%; margin-left: 120px; margin-top: 390px;">
+        <h3 class="">당신의 직업: 시민</h3>
+        <h3 class="">당신은 선량한 시민입니다. 다른 사람들과 협력해 마피아를 잡아내세요.</h3>
+        <h3 class="">마피아를 잡아내면 승리!</h3>
+      </div>  
+    </div>
+
+  </button>  
+  
+  
   <!-- 낮 투표용지 -->
   <div class="voteForm" v-if="progress.isVoteDay">
     <h3 class="dayVoteTitle">지금 낮 투표임</h3>
@@ -125,6 +178,11 @@ export default {
 
       // 시간제한
       count: 0,
+
+      // 직업정보열람용
+      isjobRollCenter: true,
+      isJobRollOpen: false,
+      job: 'citizen',      
     };
   },
   computed: {
@@ -176,6 +234,25 @@ export default {
       "saveIsCaptain",
       "saveIsConnected",
     ]),
+
+    // 직업정보열람용
+    switchJobRoll() {
+      this.isjobRollCenter = false
+      this.isJobRollOpen = false
+    },
+    openJobRoll () {
+      if (this.isjobRollCenter) {
+        this.isJobRollOpen = true
+      } else {
+      setTimeout(() => {
+        this.isJobRollOpen = true
+      }, 700)
+      if (!this.isjobRollCenter) {
+        this.isjobRollCenter = true
+      }      
+      }
+    },
+
     joinSession() {
       // --- Get an OpenVidu object ---
       this.OV = new OpenVidu();
@@ -705,5 +782,175 @@ export default {
 }
 .voteItem::-webkit-scrollbar {
   display: none;
+}
+/* 직업열람용 */
+.skipModal {
+  margin: auto;
+  position: absolute;
+  width: 70%;
+  height: 70%;
+}
+/* 결과창 Fade*/
+.voteResultFade-enter-active, .voteResultFade-leave-active {
+  transition: opacity 1s;
+}
+.voteResultFade-enter-from, .voteResultFade-leave-to {
+  opacity: 0;
+}
+.ingameDaySkipBtn {
+  position: absolute;
+  top: 70%;
+  left: 70%;
+  height: 30px;
+  border: 2px solid black;
+  border-radius: 20px;
+}
+.jobRollFormCenter {
+  position: absolute;
+  top: 30%;
+  left: 30%;
+  right: 30%;
+  /* background-color: white; */
+  opacity: 0.85;
+  width: 30vh;
+  height: 30vh;
+  margin: auto;
+  border-radius: 30px;
+  /* box-shadow: 5px 5px 5px 5px gray; */
+  background-image: url(../../public/homedesign/images/jobRoll.png);
+  background-size: 30vh 30vh;
+  background-repeat: no-repeat;
+  background-color: rgba( 255, 255, 255, 0 );
+  transition-duration: 0.5s;
+  transition-timing-function: ease;
+  z-index: 1199;
+}
+.jobRollFormSide {
+  position: absolute;
+  top: 10%;
+  left: 90%;
+  right: 30%;
+  /* background-color: white; */
+  opacity: 0.85;
+  width: 10vh;
+  height: 10vh;
+  margin: auto;
+  border-radius: 30px;
+  /* box-shadow: 5px 5px 5px 5px gray; */
+  background-image: url(../../public/homedesign/images/jobRoll.png);
+  background-size: 10vh 10vh;
+  background-repeat: no-repeat;
+  background-color: rgba( 255, 255, 255, 0 );
+  transition-duration: 0.5s;
+  transition-timing-function: ease
+}
+.openedJobRoll {
+  position: absolute;
+  top: 5%;
+  left: 10%;
+  right: 10%;
+  /* background-color: white; */
+  opacity: 1;
+  width: 80vh;
+  height: 90vh;
+  margin: auto;
+  border-radius: 30px;
+  /* box-shadow: 5px 5px 5px 5px gray; */
+  background-image: url(../../public/homedesign/images/jobinfo.png);
+  background-size: 80vh 90vh;
+  background-repeat: no-repeat;
+  background-color: rgba( 255, 255, 255, 0 );
+  z-index: 1200;
+}
+.voteForm {
+  position: absolute;
+  top: 10%;
+  left: 10%;
+  right: 10%;
+  /* background-color: white; */
+  opacity: 0.7;
+  width: 80vh;
+  height: 80vh;
+  margin: auto;
+  border-radius: 30px;
+  /* box-shadow: 5px 5px 5px 5px gray; */
+  background-image: url(../../public/homedesign/images/vote_paper.png);
+  background-size: 80vh 80vh;
+  background-repeat: no-repeat;
+}
+.ingameBackground {
+  background-image: url(../../public/homedesign/images/wait_mafia.gif);
+  height: 100vh;
+  background-size: cover;
+  background-repeat: no-repeat;
+  /* height: 773.3px; */
+}
+.dayVoteTitle {
+  text-align: center;
+  margin-top: 50px;
+}
+.mafiaImage {
+  position: absolute;
+  top: 0%;
+  left: 20%;
+  right: 30%;
+  opacity: 1;
+  width: 50vh;
+  height: 50vh;
+  margin: auto;
+  border-radius: 30px;
+  background-size: 50vh 50vh;
+  background-image: url(../../public/homedesign/images/mafia_whiteicon.png);
+  background-repeat : no-repeat;
+  background-size: 100% 100%;
+  margin-bottom:px;  
+}
+.citizenImage {
+  position: absolute;
+  top: 0%;
+  left: 20%;
+  right: 30%;
+  opacity: 1;
+  width: 50vh;
+  height: 50vh;
+  margin: auto;
+  border-radius: 30px;
+  background-size: 50vh 50vh;  
+  background-image: url(../../public/homedesign/images/citizen_whiteicon.png);
+  background-repeat : no-repeat;
+  background-size: 100% 100%;
+  margin-bottom:px;
+}
+.doctorImage {
+  position: absolute;
+  top: 0%;
+  left: 20%;
+  right: 30%;
+  opacity: 1;
+  width: 50vh;
+  height: 50vh;
+  margin: auto;
+  border-radius: 30px;
+  background-size: 50vh 50vh;  
+  background-image: url(../../public/homedesign/images/doctor_whiteicon.png);
+  background-repeat : no-repeat;
+  background-size: 100% 100%;
+  margin-bottom:px;
+}
+.headmasterImage {
+  position: absolute;
+  top: 0%;
+  left: 20%;
+  right: 30%;
+  opacity: 1;
+  width: 50vh;
+  height: 50vh;
+  margin: auto;
+  border-radius: 30px;
+  background-size: 50vh 50vh;
+  background-image: url(../../public/homedesign/images/headmaster_whiteicon.png);
+  background-repeat : no-repeat;
+  background-size: 100% 100%;
+  margin-bottom:px;  
 }
 </style>
