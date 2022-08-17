ff165d2ae557f896e7e80b41f02f53c2da66894e
leeghee282 <lghee282@gmail.com>
2022-08-17 14:39:45 +0900

    1

--- frontend/src/store/modules/roomdataStore.js
+++ frontend/src/store/modules/roomdataStore.js
@@ -1,27 +1,22 @@
 // 참여중인 방 제목, 이 유저가 방장인지 체크할 예정
 // import { createStore } from 'vuex'
 // import { reactive } from "vue";
-import {
-  roomMake,
-  roomDelete,
-  roomEnter,
-} from "@/api/room.js";
-import router from '@/router'
-
+import { roomMake, roomDelete, roomEnter } from "@/api/room.js";
+import router from "@/router";
 
 const roomdataStore = {
   namespaced: true,
   state: {
     roomNo: -1,
-    roomTitle: '뭐임?',
+    roomTitle: "뭐임?",
     isCaptain: false,
-    password: '',
+    password: "",
     isConnected: false,
   },
   getters: {
     password(state) {
       return state.password;
-    },    
+    },
     roomNo(state) {
       return state.roomNo;
     },
@@ -36,44 +31,48 @@ const roomdataStore = {
     },
   },
   mutations: {
-    SET_PASSWORD: (state, password) => state.password = password,    
-    SET_ROOMNO: (state, roomNo) => state.roomNo = roomNo,
-    SET_ROOMTITLE: (state, roomTitle) => state.roomTitle = roomTitle,
-    SET_ISCAPTAIN: (state, isCaptain) => state.isCaptain = isCaptain,
-    SET_ISCONNECTED: (state, isConnected) => state.isConnected = isConnected,
+    SET_PASSWORD: (state, password) => (state.password = password),
+    SET_ROOMNO: (state, roomNo) => (state.roomNo = roomNo),
+    SET_ROOMTITLE: (state, roomTitle) => (state.roomTitle = roomTitle),
+    SET_ISCAPTAIN: (state, isCaptain) => (state.isCaptain = isCaptain),
+    SET_ISCONNECTED: (state, isConnected) => (state.isConnected = isConnected),
   },
   actions: {
     savePassword({ commit }, password) {
-      commit('SET_PASSWORD', password)
-    },    
+      commit("SET_PASSWORD", password);
+    },
     saveRoomNo({ commit }, roomNo) {
-      commit('SET_ROOMNO', roomNo)
+      commit("SET_ROOMNO", roomNo);
     },
     saveIsCaptain({ commit }, isCaptain) {
-      commit('SET_ISCAPTAIN', isCaptain)
+      commit("SET_ISCAPTAIN", isCaptain);
     },
     saveRoomTitle({ commit }, roomTitle) {
-      commit('SET_ROOMTITLE', roomTitle)
+      commit("SET_ROOMTITLE", roomTitle);
     },
     saveIsConnected({ commit }, isConnected) {
-      commit('SET_ISCONNECTED', isConnected)
-    },    
+      commit("SET_ISCONNECTED", isConnected);
+    },
     // 외부요청
     async makeRoom({ commit }, roomInfo) {
       console.log(roomInfo);
       await roomMake(
         roomInfo,
         (response) => {
-          console.log('============================')
-          console.log('방만들기 성공')
-          console.log(response.data)
-          commit('SET_ROOMTITLE', response.data.roomTitle)
-          commit('SET_ISCAPTAIN', true)
-          console.log(response.data.roomTitle)
-          console.log(response.data.no)
-          router.push({ name: 'wait', params: { roomnumber:  response.data.no } })
+          console.log("============================");
+          console.log("방만들기 성공");
+          console.log(response.data);
+          commit("SET_ROOMNO", response.data.no);
+          commit("SET_ROOMTITLE", response.data.roomTitle);
+          commit("SET_ISCAPTAIN", true);
+          console.log(response.data.roomTitle);
+          console.log(response.data.no);
+          router.push({
+            name: "wait",
+            params: { roomnumber: response.data.no },
+          });
         },
-        () => {},
+        () => {}
       );
     },
 
@@ -81,10 +80,10 @@ const roomdataStore = {
       await roomDelete(
         roomNo,
         () => {
-          commit('SET_ROOMTITLE', '')
-          commit('SET_ISCAPTAIN', false)
+          commit("SET_ROOMTITLE", "");
+          commit("SET_ISCAPTAIN", false);
         },
-        () => {},
+        () => {}
       );
     },
 
@@ -93,27 +92,25 @@ const roomdataStore = {
       await roomEnter(
         roomInfo,
         () => {
-          console.log('========응답오나=======')
-          commit('SET_ROOMNO', roomInfo.no)
-          commit('SET_PASSWORD', roomInfo.password)
-          router.push({ name: 'wait', params: { roomnumber:  roomInfo.no } })
+          console.log("========응답오나=======");
+          commit("SET_ROOMNO", roomInfo.no);
+          commit("SET_PASSWORD", roomInfo.password);
+          router.push({ name: "wait", params: { roomnumber: roomInfo.no } });
         },
-        () => {},
+        () => {}
       );
     },
   },
 };
 
-
 export default roomdataStore;
-
 
 // export default createStore({
 //   state: {
 //     roomTitle: "asdf",
 //     isCaptain: "",
 //   },
-  
+
 //   getters: { // return값이 있는 함수 형태로 작성
 //     roomTitle(state) {
 //       return state.roomTitle + '';
@@ -122,23 +119,22 @@ export default roomdataStore;
 //       return state.isCaptain + '';
 //     },
 //   },
-  
+
 //   actions: { // 비순차적, 비동기 로직 사용
 //     saveRoomTitle({ commit }, roomTitle) {
 //       commit('SET_ROOMTITLE', roomTitle)
 //     },
-  
+
 //     saveIsCaptain({ commit }, isCaptain) {
 //       commit('SET_ISCAPTAIN', isCaptain)
 //     },
 //   },
-  
+
 //   mutations: { // state의 값에 접근하여 그 값을 바꿔줌
 //     SET_ROOMTITLE: (state, roomTitle) => state.roomTitle = roomTitle,
 //     SET_ISCAPTAIN: (state, isCaptain) => state.isCaptain = isCaptain,
 //   },
 // })
-
 
 // const state = {
 //   roomTitle: "",
